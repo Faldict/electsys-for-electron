73563ca84d0a5215cf70a0f5190dce46e09bda01
Faldict <legadoubledyan@gmail.com>
2016-05-26 08:55:18 +0800

    包装了个外壳
    我怎么可以这么机智

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  win.loadURL(`http://electsys.sjtu.edu.cn`);
+  win.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
   win.webContents.openDevTools();
